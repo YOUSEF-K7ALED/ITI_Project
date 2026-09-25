a2baa8d5cb69fad5bdf33127687aa0f33d2f3517
liiii-ii <2400716@eng.asu.edu.eg>
2026-09-25 11:41:52 +0300

    events module with:CURD,search,filters,pagination

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,3 +4,16 @@
 //     - Connect using process.env.MONGO_URI
 //     - Log a success message with the connected host
 //     - On failure: log the error and process.exit(1)
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB; 
--- backend/src/models/Event.js
+++ backend/src/models/Event.js
@@ -15,3 +15,31 @@
 // Notes:
 //   - availableSeats = capacity - bookedSeats (can be a virtual field)
 //   - Used for Search & Filter (title, category, location, price, date)
+
+
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+
+title:{ type: String, required: true , trim: true },
+description:{ type: String, required: true , trim: true },
+category:{ type: String, required: true , trim: true ,enum: ["Concert", "Workshop", "Course", "Sports Event", "Conference" ,"Arts Event"]},
+location:{ type: String, required: true , trim: true },
+date:{ type: Date, required: true },
+time:{ type: String, trim: true },
+price:{ type: Number, required: true , min: 0 },
+capacity:{ type: Number, required: true , min: 1 },
+bookedSeats:{ type: Number, default: 0 , min: 0 },
+createdBy:{ type: mongoose.Schema.Types.ObjectId, ref: "User" } ,
+
+},{ timestamps: true });
+
+eventSchema.virtual('availableSeats').get(function() {
+  return this.capacity - this.bookedSeats;
+})
+
+
+eventSchema.set('toJSON', { virtuals: true });
+
+
+module.exports = mongoose.model('Event', eventSchema);
