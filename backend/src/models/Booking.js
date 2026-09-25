5524d91414f04179b34bd86a397cac7a611ed7a4
Bedro <badranny@gmail.com>
2026-09-25 15:46:53 +0200

    Create booking model

--- backend/src/models/Booking.js
+++ backend/src/models/Booking.js
@@ -9,3 +9,38 @@
 // Notes:
 //   - Add a compound index/check in controller logic to prevent
 //     a user from booking the same event twice (duplicate booking rule)
+const mongoose = require("mongoose");
+
+const bookingSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    event: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Event",
+      required: true,
+    },
+
+    status: {
+      type: String,
+      enum: ["confirmed", "cancelled"],
+      default: "confirmed",
+    },
+
+    bookingDate: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true },
+);
+
+// The combination of user + event must be unique to prevent booking the same event more than one time for the user
+bookingSchema.index({ user: 1, event: 1 }, { unique: true });
+
+const Booking = mongoose.model("Booking", bookingSchema);
+
+module.exports = Booking;
